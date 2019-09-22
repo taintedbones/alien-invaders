42628d26c999a1dc43d23ad1ec70a992a2d2aa62
taintedbones <kierstyn.erin@gmail.com>
2019-09-21 23:43:42 -0700

    Added functionality for firing bullets

---
+++ game_function.py
@@ -0,0 +1,37 @@
+import sys
+import pygame
+from bullet import Bullet
+
+
+def check_keydown_events(event, settings, screen, ship, bullets):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+    if event.key == pygame.K_SPACE:
+        new_bullet = Bullet(settings, screen, ship)
+        bullets.add(new_bullet)
+
+
+def check_events(settings, screen, ship, bullets):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+            return True
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, settings, screen, ship, bullets)
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = False
+    return False
+
+
+def update_screen(settings, screen, ship, bullets):
+    screen.fill(settings.bg_color)
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+    ship.blitme()
+
+    pygame.display.flip()
